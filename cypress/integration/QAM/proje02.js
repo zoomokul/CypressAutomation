0b1207ce1d842735d98997c62ebcb1a5058183e6
ahmetb <zoomokul@gmail.com>
2021-05-07 18:44:07 +0300

    mayis7

--- cypress/integration/QAM/proje02.js
+++ cypress/integration/QAM/proje02.js
@@ -1,27 +1,34 @@
 describe('Loops', () => {
   
           it('third', () => {
-              cy.visit("https://www.memurlar.net/")
-             
-              cy.get('#GlobalHeader > div.Main > div')
-                .each((el)=>{
-            
-//bu bir yorum
+              cy.visit("http://automationpractice.com/index.php ")
+             // cy.get('.menu-content>li').first().click()
 
-                //cy.log(el.text())
-                cy.get(el).click()
-                })
-                
+              //ürün ekleme
+              //cy.get('a[data-id-product=1]').first().click()
+              //cy.get("span[title='Continue shopping']").click()
+
+              var i;
+            for (i = 1; i < 7; i++) {
+              cy.get('a[data-id-product='+i+']').first().click()
+              cy.get("span[title='Continue shopping']").click()
+            }
+            cy.get("a[title='View my shopping cart']").click()
            
-           
-           
+           /*
+            1. yol  tüm eklediklerimi kaldırdı
+            cy.get('.cart_quantity_delete').click({ multiple: true }) 
+            
+            */
+            //2. yol
+            cy.get('.cart_quantity_delete').each((el)=>{
+              cy.get(el).click()
+            })
+
+
           
-              
-  
-  
-  
-        
-     })
+    
+            })
     
     
   
